1dd5c744909adbb59b82bed786ad52879de9ba88
quantxz <indiedev.core@gmail.com>
2024-03-11 16:41:35 -0300

    firts commit

---
+++ js/api/login-validation.js
@@ -0,0 +1,70 @@
+const url = new URLSearchParams(window.location.search)
+const username = url.get("username")
+const password = url.get("password")
+const email = url.get("email")
+
+const apiRequest = async () => {
+    const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTAxODEzODAsImV4cCI6MTcxMjg1OTc4MH0.j51dCOshP0T6J0xbX9nP6UYNKGqcHFJbyX6YWPFNEtg";
+
+    if (username !== "" && password !== "" && email !== "") {
+        try {
+            const response = await fetch("https://tcc-u2qf.onrender.com/users/login", {
+                method: "POST",
+                body: JSON.stringify({
+                    nickname: username,
+                    password: password,
+                    email: email
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": authToken
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                window.location.href = "../../html/home.html";
+            } else {
+                const data = await response.json();
+                switch (true) {
+                    case data.message === "Erro de validação":
+                    case data.status === 400 && data.message === "dados do usuario incorretos":
+                        if (
+                            data.errors[0].constraints.matches &&
+                            data.errors[0].constraints.matches.includes("a senha deve conter uma letra maiuscula, uma letra minuscula e um caractere especial")
+                        ) {
+                            window.location.href = "../../html/login.html?error&errorID=153032";
+                        } else if (data.errors[0].constraints.isEmail && data.errors[0].constraints.isEmail.includes("Formato de email Invalido")) {
+                            window.location.href = "../../html/login.html?error&errorID=153031";
+                        } else if (
+                            data.errors[0].constraints.matches &&
+                            data.errors[0].constraints.matches.includes("Formato de email Invalido") &&
+                            data.errors[0].constraints.matches.includes("a senha deve conter uma letra maiuscula, uma letra minuscula e um caractere especial")
+                        ) {
+                            window.location.href = "../../html/login.html?error&errorID=153030";
+                        }
+                        break;
+                
+                    case (data.status === 400 && data.message === "Internal Server Error"):
+                        window.location.href = "../../html/errors/404-page.html";
+                        break;
+                
+                
+                    case (data.status === 500 && data.message === "Internal Server Error"):
+                        console.log("aba")   
+                        window.location.href = "../../html/login.html?error&errorID=153034"
+                        break;
+                
+                    default:
+                        window.location.href = "../../html/login.html";
+                        break;
+                }
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            // Lidar com erro na requisição
+        }
+    }
+}
+
+apiRequest();
---
+++ js/login/login.js
@@ -0,0 +1,50 @@
+const userInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const emailInput = document.querySelector("#email");
+const submitInput = document.querySelector("#submit");
+const form = document.querySelector("#form");
+const emailErrorElement = document.querySelector("#emailError");
+const passwordErrorElement = document.querySelector("#passwordError");
+const usernameErrorElement = document.querySelector("#usernameError");
+
+
+const url = new URLSearchParams(window.location.search)
+const errorId = url.get("errorID")
+
+
+switch (true) {
+    case errorId === "153032": 
+        passwordErrorElement.textContent = "Senha invalida";
+        passwordErrorElement.style.display = "block"
+        break;
+    case errorId === "153031":
+        emailErrorElement.textContent = "Email Invalido";
+        emailErrorElement.style.display = "block";
+        break;
+    case errorId === "153030":
+        passwordErrorElement.textContent = "Senha e email invalidos";
+        passwordErrorElement.style.display = "block"
+        emailErrorElement.textContent = "Senha e email invalidos";
+        emailErrorElement.style.display = "block";
+        break;
+    case errorId === "153034":
+        usernameErrorElement.textContent = "Usuario ou email invalidos";
+        usernameErrorElement.style.display = "block"
+        emailErrorElement.textContent = "Usuario ou email invalidos";
+        emailErrorElement.style.display = "block";
+        break;
+    default:
+        console.log("Outro erro não tratado");
+}
+
+form.addEventListener("input", () => {
+    const isUserValid = userInput.value.length >= 7 && userInput.value.length <= 55;
+    const isPasswordValid = passwordInput.value.length >= 7 && passwordInput.value.length <= 55;
+    const isEmailValid = emailInput.value.length >=  7 && emailInput.value.length <= 55;
+
+    if (isUserValid && isPasswordValid && isEmailValid) {
+        submitInput.disabled = false;
+    } else {
+        submitInput.disabled = true;
+    }
+});
